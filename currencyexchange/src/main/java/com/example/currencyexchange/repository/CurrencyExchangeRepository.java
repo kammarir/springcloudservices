4e685b61ba423956f8c8d63a34b285ed6f09d14c
kammarir <75215745+kammarir@users.noreply.github.com>
2021-04-03 21:48:38 -0500

    Delete
    testy

--- currencyexchange/src/main/java/com/example/currencyexchange/repository/CurrencyExchangeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.currencyexchange.repository;
-
-import com.example.currencyexchange.model.CurrencyExchange;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
-
-    CurrencyExchange findByFromAndTo(String from,String to);
-}
